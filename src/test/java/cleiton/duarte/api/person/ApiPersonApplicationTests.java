55309d18421ae5eea1aef589368201555ed82415
cleiton <djcleitonduarte@hotmail.com>
2021-07-04 08:19:50 -0400

    Project starded

---
+++ src/test/java/cleiton/duarte/api/person/ApiPersonApplicationTests.java
@@ -0,0 +1,13 @@
+package cleiton.duarte.api.person;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPersonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
